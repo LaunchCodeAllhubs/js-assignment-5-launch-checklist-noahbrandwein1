b4a3fb73d901919081cf4414550d45c313ebebeb
noah.brandwein1 <noah.brandwein98@gmail.com>
2023-08-10 19:59:21 -0500

    more validation to make sure there is input in fields.

--- script.js
+++ script.js
@@ -23,7 +23,16 @@ window.addEventListener("load", function () {
         const coPilotName = document.querySelector('#copilotName').value;
         const fuelLevel = document.querySelector('#fuelLevel').value;
         const cargoMass = document.querySelector('#cargoMass').value;
-        
+
+        if (pilotName === '' || coPilotName === '' || fuelLevel === '' || cargoMass === '') {
+            alert('All fields are required!');
+            return;
+        }
+
+        if (!isNaN(pilotName) || !isNaN(coPilotName) || isNaN(fuelLevel) || isNaN(cargoMass)) {
+            alert('Please enter valid information for all field.');
+            return;
+        }
 
     })
 });
