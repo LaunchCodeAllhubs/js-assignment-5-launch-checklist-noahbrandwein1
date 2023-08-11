c17da16d4b8ddc864ee9615b0b748fd508c15a33
noah.brandwein1 <noah.brandwein98@gmail.com>
2023-08-10 19:50:00 -0500

    Prevent Default behavior and Validation and list Update

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
 // Write your JavaScript code here!
 
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
 
-   let listedPlanets;
-   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
-   listedPlanetsResponse.then(function (result) {
-       listedPlanets = result;
-       console.log(listedPlanets);
-   }).then(function () {
-       console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
-   
+    let listedPlanets;
+    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
+    let listedPlanetsResponse;
+    listedPlanetsResponse.then(function (result) {
+        listedPlanets = result;
+        console.log(listedPlanets);
+    }).then(function () {
+        console.log(listedPlanets);
+        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+        pickPlanet(listedPlantets);
+    })
+
+    const launchForm = document.querySelector('#launchForm');
+
+    launchForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        const pilotName = document.querySelector('#pilotName').value;
+        const coPilotName = document.querySelector('#copilotName').value;
+        const fuelLevel = document.querySelector('#fuelLevel').value;
+        const cargoMass = document.querySelector('#cargoMass').value;
+        
+
+    })
 });
