92fe318ac98259360b411a24cf70577d01dbd3f0
noah.brandwein1 <noah.brandwein98@gmail.com>
2023-08-13 13:18:21 -0500

    moved some things around

--- script.js
+++ script.js
@@ -11,27 +11,28 @@ window.addEventListener("load", function () {
         listedPlanets = result;
         console.log(listedPlanets);
     }).then(function () {
-    // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-    let selectedPlanet = pickPlanet(listedPlanets);
+        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+        let selectedPlanet = pickPlanet(listedPlanets);
         addDestinationInfo(document, selectedPlanet.name, selectedPlanet.diameter, selectedPlanet.star, selectedPlanet.distance, selectedPlanet.moons, selectedPlanet.image);
-        console.log(listedPlanets);
+
     });
-});
 
-document.getElementById("formSubmit").addEventListener("click", function (event) {
-    event.preventDefault();
+    document.getElementById("formSubmit").addEventListener("click", function (event) {
+        event.preventDefault();
 
-    const pilotName = document.querySelector("input[name='pilotName']").value;
-    const copilotName = document.querySelector("input[name='copilotName']").value;
-    const fuelLevel = document.querySelector("input[name='fuelLevel']").value;
-    const cargoMass = document.querySelector("input[name='cargoMass']").value;
+        const pilotName = document.querySelector("input[name='pilotName']").value;
+        const copilotName = document.querySelector("input[name='copilotName']").value;
+        const fuelLevel = document.querySelector("input[name='fuelLevel']").value;
+        const cargoMass = document.querySelector("input[name='cargoMass']").value;
 
-    if (pilotName === "" || copilotName === "" || fuelLevel === "" || cargoMass === "") {
-        alert("All fields are required!");
-        return;
+        if (pilotName === "" || copilotName === "" || fuelLevel === "" || cargoMass === "") {
+            alert("All fields are required!");
+            return;
 
-    }
+        }
 
-    formSubmission(document, listedPlanets, pilotName, copilotName, fuelLevel, cargoMass);
+        formSubmission(document, listedPlanets, pilotName, copilotName, fuelLevel, cargoMass);
+
+    });
 
 });
